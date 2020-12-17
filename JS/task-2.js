795249b0a64521eb02f4ee230b8df615b7ba571f
Oleksii Shcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2020-12-17 13:31:33 +0200

    upd

--- JS/task-2.js
+++ JS/task-2.js
@@ -1,20 +1,21 @@
+
 const inventory = {
   items: ['Knife', 'Gas mask'],
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
-
-    inventory.items.push(itemName);
+    
+    this.items.push(itemName);
   },
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
 
-    inventory.items = inventory.items.filter(item => item !== itemName);
+    this.items = this.items.filter(item => item !== itemName);
   },
 };
 
 const invokeInventoryAction = function(itemName, action) {
   console.log(`Invoking action on ${itemName}`);
-  action(itemName);
+  action.call(inventory, itemName);
 };
 
 invokeInventoryAction('Medkit', inventory.add);
